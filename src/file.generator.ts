1ce6476cd62c94fdeb62145775cd19814d862405
ferhado <gmail@ferhado.com>
2023-11-14 17:14:15 +0100

    fix: check style extention supporting

--- src/file.generator.ts
+++ src/file.generator.ts
@@ -1,8 +1,8 @@
-import * as fs from "fs";
-import path from "path";
-import * as vscode from "vscode";
-import { fileConfigFileInterface } from "./files.config";
-import { loadTemplate, toCamelCase } from "./utils";
+import * as fs from 'fs';
+import path from 'path';
+import * as vscode from 'vscode';
+import { SUPPORTED_STYLE_EXTENSIONS, fileConfigFileInterface } from './files.config';
+import { loadTemplate, toCamelCase } from './utils';
 
 export function generateFileContent(directoryPath: string, baseName: string, action: any, angularConfig: any): void {
   const resourcePath = /^(Component|Module)$/.test(action.command) ? path.join(directoryPath, baseName) : directoryPath;
@@ -12,27 +12,32 @@ export function generateFileContent(directoryPath: string, baseName: string, act
   }
 
   action.files.forEach((source: fileConfigFileInterface) => {
-    let content = "";
+    let content = '';
     let fileExt = source.ext;
-    let styleExt = angularConfig.style ?? "scss";
+    let styleExt = angularConfig.style ?? 'scss';
 
-    if (fileExt === "scss") {
+    if (!SUPPORTED_STYLE_EXTENSIONS.includes(styleExt)) {
+      vscode.window.showWarningMessage(`Unsupported style extension "${styleExt}". Defaulting to "scss".`);
+      styleExt = 'scss';
+    }
+
+    if (fileExt === 'scss') {
       fileExt = styleExt;
     }
 
     let templatePath = `${source.file}.${fileExt}`;
-    let fileName = source.file === "index" ? templatePath : `${baseName}.${templatePath}`;
+    let fileName = source.file === 'index' ? templatePath : `${baseName}.${templatePath}`;
     const filePath = path.join(resourcePath, fileName);
 
-    if (action.command === "InlineComponent") {
-      templatePath = "inline.component.ts";
-    } else if (action.command === "Module" && /(component|service)\.ts$/.test(templatePath)) {
+    if (action.command === 'InlineComponent') {
+      templatePath = 'inline.component.ts';
+    } else if (action.command === 'Module' && /(component|service)\.ts$/.test(templatePath)) {
       templatePath = `module.${templatePath}`;
     }
 
     const className = toCamelCase(baseName);
     content = source.content ?? loadTemplate(templatePath);
-    content = content.replace(/%APP_PREFIX%/g, angularConfig.prefix ?? "app");
+    content = content.replace(/%APP_PREFIX%/g, angularConfig.prefix ?? 'app');
     content = content.replace(/%FILE_NAME%/g, baseName);
     content = content.replace(/%CLASS_NAME%/g, className);
     content = content.replace(/%STYLE_EXT%/g, styleExt);
--- src/files.config.ts
+++ src/files.config.ts
@@ -1,3 +1,5 @@
+export const SUPPORTED_STYLE_EXTENSIONS = ['css', 'scss', 'sass', 'less'];
+
 export interface fileConfigFileInterface {
   file: string;
   ext: string;
@@ -12,48 +14,48 @@ export interface fileConfigInterface {
 
 export const fileConfig: fileConfigInterface[] = [
   {
-    title: "Component",
-    command: "InlineComponent",
-    files: [{ file: "component", ext: "ts" }]
+    title: 'Component',
+    command: 'InlineComponent',
+    files: [{ file: 'component', ext: 'ts' }]
   },
 
   {
-    title: "Component",
-    command: "Component",
+    title: 'Component',
+    command: 'Component',
     files: [
-      { file: "component", ext: "ts" },
-      { file: "component", ext: "html" },
-      { file: "component", ext: "scss" },
-      { file: "index", ext: "ts", content: "export * from './%FILE_NAME%.component';" }
+      { file: 'component', ext: 'ts' },
+      { file: 'component', ext: 'html' },
+      { file: 'component', ext: 'scss' },
+      { file: 'index', ext: 'ts', content: "export * from './%FILE_NAME%.component';" }
     ]
   },
 
   {
-    title: "Module",
-    command: "Module",
+    title: 'Module',
+    command: 'Module',
     files: [
-      { file: "module", ext: "ts" },
-      { file: "component", ext: "ts" },
-      { file: "service", ext: "ts" },
-      { file: "component", ext: "html" },
-      { file: "component", ext: "scss" },
+      { file: 'module', ext: 'ts' },
+      { file: 'component', ext: 'ts' },
+      { file: 'service', ext: 'ts' },
+      { file: 'component', ext: 'html' },
+      { file: 'component', ext: 'scss' },
       {
-        file: "index",
-        ext: "ts",
+        file: 'index',
+        ext: 'ts',
         content: "export * from './%FILE_NAME%.module';\nexport * from './%FILE_NAME%.service';"
       }
     ]
   },
 
   {
-    title: "Service",
-    command: "Service",
-    files: [{ file: "service", ext: "ts" }]
+    title: 'Service',
+    command: 'Service',
+    files: [{ file: 'service', ext: 'ts' }]
   },
 
   {
-    title: "Directive",
-    command: "Directive",
-    files: [{ file: "directive", ext: "ts" }]
+    title: 'Directive',
+    command: 'Directive',
+    files: [{ file: 'directive', ext: 'ts' }]
   }
 ];
