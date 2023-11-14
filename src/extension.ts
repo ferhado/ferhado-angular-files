00c4017d388f27b845e8b316d20920bc0ae6467b
ferhado <gmail@ferhado.com>
2023-11-14 16:37:17 +0100

    refactor: remove console.log

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,9 @@
-import * as vscode from "vscode";
-import { openCommandDialog } from "./command-dialog";
-import { fileConfig } from "./files.config";
-import { showRenameComponentDialog } from "./rename-component";
+import * as vscode from 'vscode';
+import { openCommandDialog } from './command-dialog';
+import { fileConfig } from './files.config';
+import { showRenameComponentDialog } from './rename-component';
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log(context);
   for (let config of fileConfig) {
     let disposable = vscode.commands.registerCommand(`extension.ngfFilesNew${config.command}`, (uri: vscode.Uri) =>
       openCommandDialog(config, uri)
